009619c0872d70d29ac7d94b39fabcc9f9e2731f
Antonin <arkanen@gmail.com>
2022-10-26 16:39:42 +0200

    Moved modal close btn style to style-when-not-using-bootstrap-5.css, icon stroke = currentColor

--- Form/UxHorizontalCollectionType.php
+++ Form/UxHorizontalCollectionType.php
@@ -9,14 +9,14 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class UxHorizontalCollectionType extends UxCollectionType
 {
 
-    public const ICON_LEFT = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    public const ICON_LEFT = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-left" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
               <line x1="5" y1="12" x2="19" y2="12" />
               <line x1="5" y1="12" x2="9" y2="16" />
               <line x1="5" y1="12" x2="9" y2="8" />
             </svg>';
 
-    public const ICON_RIGHT = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    public const ICON_RIGHT = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <line x1="5" y1="12" x2="19" y2="12" />
           <line x1="15" y1="16" x2="19" y2="12" />
